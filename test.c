d8d9d82f36c88e9455f1cf464c46dcd70067c11e
ukashu <lukaszfilipiuk123@gmail.com>
2025-11-13 14:24:23 +0100

    +color

--- test.c
+++ test.c
@@ -31,6 +31,9 @@ int main(void) {
 
     while(!glfwWindowShouldClose(window))
     {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
